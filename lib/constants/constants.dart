ec99203bec1b795bfd1d90e47764db2a12c8f29c
nazeemnato <nazeemnob18@gmail.com>
2021-02-12 09:56:46 +0530

    :)

---
+++ lib/constants/constants.dart
@@ -0,0 +1,28 @@
+ var randomResponse = [
+        "Not so sure",
+        "It is decidedlly",
+        "definitely",
+        "Most likely",
+        "Absolutely not",
+        "Outlook is good",
+        "I see good things happening",
+        "Never",
+        "Negative",
+        "Could be",
+        "Yes",
+        "No",
+        "Possible, but not probable",
+        "Let\'s Hope"
+      ];
+      var negativeResponse = [
+        "Maybe",
+        "Outlook is good",
+        "Never",
+        "Negative",
+        "Could be",
+        "Unclear, ask again",
+        "Yes",
+        "No",
+        "Possible, but not probable",
+        "No idea"
+      ];
