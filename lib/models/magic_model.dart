61376b8310ccd4d5c4172e3ed16f2ec28b61e1b8
nazeemnato <nazeemnob18@gmail.com>
2021-02-18 22:06:54 +0530

    magic models

---
+++ lib/models/magic_model.dart
@@ -0,0 +1,11 @@
+import 'package:meta/meta.dart';
+
+class Magic {
+  final String question;
+  final String response;
+
+  Magic({
+    @required this.question,
+    @required this.response
+  });
+}
